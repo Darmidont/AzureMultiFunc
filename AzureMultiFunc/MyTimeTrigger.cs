3618bd26b1f94f41296299060132a95f58a884be
Darmidont <ddarmidont@gmail.com>
2026-01-13 13:31:14 +0200

    add connect to sql server

--- AzureMultiFunc/MyTimeTrigger.cs
+++ AzureMultiFunc/MyTimeTrigger.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging;
 
 namespace AzureMultiFunc;
@@ -17,7 +18,18 @@ public class MyTimeTrigger
     public void Run([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer)
     {
         _logger.LogInformation("C# Timer trigger function executed at: {executionTime}", DateTime.Now);
-        
+
+        string connStr = Environment.GetEnvironmentVariable("SqlConnectionString");
+        if (!string.IsNullOrEmpty(connStr))
+        {
+            _logger.LogInformation($"Found connection: {connStr}");
+            using SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("SELECT TOP 1 name FROM sys.tables", conn);
+            var result = cmd.ExecuteScalar();
+            _logger.LogInformation($"First table: {result}");
+        }
+
         if (myTimer.ScheduleStatus is not null)
         {
             _logger.LogInformation("Next timer schedule at: {nextSchedule}", myTimer.ScheduleStatus.Next);
